8333d8e7c69389cdd2033d2a1ea23a40f72b6f1e
yonatanBtl <yonatan.bartolo@unmsm.edu.pe>
2023-11-14 23:09:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/inventory/data/AppContainer.kt
@@ -0,0 +1,18 @@
+
+
+package com.example.inventory.data
+
+import android.content.Context
+
+
+interface AppContainer {
+    val itemsRepository: ItemsRepository
+}
+
+
+class AppDataContainer(private val context: Context) : AppContainer {
+
+    override val itemsRepository: ItemsRepository by lazy {
+        OfflineItemsRepository(InventoryDatabase.getDatabase(context).itemDao())
+    }
+}
---
+++ app/src/main/java/com/example/inventory/data/ItemsRepository.kt
@@ -0,0 +1,21 @@
+
+package com.example.inventory.data
+
+import kotlinx.coroutines.flow.Flow
+
+
+interface ItemsRepository {
+
+    fun getAllItemsStream(): Flow<List<Item>>
+
+
+    fun getItemStream(id: Int): Flow<Item?>
+
+
+    suspend fun insertItem(item: Item)
+
+    suspend fun deleteItem(item: Item)
+
+    suspend fun updateItem(item: Item)
+}
+
